f96fb0c429526216ccc0225cbd8a6d8e8300a7fd
ShaneUnknown <shanejalma@gmail.com>
2026-02-01 14:04:45 -0500

    fixed padding bottom BodyList.css

--- src/zones/body/ListBody.jsx
+++ src/zones/body/ListBody.jsx
@@ -1,17 +1,18 @@
 import DefaultList from "./DefaultList"
 import SearchList from "./SearchList"
 import { useMain } from "../../context/MainContext"
+import './ListBody.css'
 
 const ListBody = () => {
   const { searchtext } = useMain()
   return (
-    <>
+    <div class='ListBody'>
     {
       searchtext.length > 0 ?
       <SearchList /> :
       <DefaultList />
     }
-    </>
+    </div>
   )
 }
 
